07fe203f45f75fa4e70b8e30de09dc59a062a40e
MarcRatayzyk <marc.rzyk@gmail.com>
2026-06-08 20:09:16 +0200

    Replace message icons with SMS PNG and fix production deploy

--- resources/js/Components/UiIcon.vue
+++ resources/js/Components/UiIcon.vue
@@ -1,4 +1,6 @@
 <script setup>
+import smsIconUrl from '../../images/sms.png';
+
 const props = defineProps({
     name: {
         type: String,
@@ -9,6 +11,17 @@ const props = defineProps({
         default: 'h-6 w-6',
     },
 });
+
+const smsMaskStyle = {
+    WebkitMaskImage: `url(${smsIconUrl})`,
+    maskImage: `url(${smsIconUrl})`,
+    WebkitMaskRepeat: 'no-repeat',
+    maskRepeat: 'no-repeat',
+    WebkitMaskPosition: 'center',
+    maskPosition: 'center',
+    WebkitMaskSize: 'contain',
+    maskSize: 'contain',
+};
 </script>
 
 <template>
@@ -57,21 +70,13 @@ const props = defineProps({
             d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664M6.75 7a.75.75 0 01-.75-.75v-1.5A2.25 2.25 0 019 3h6a2.25 2.25 0 012.25 2.25v1.5a.75.75 0 01-.75.75M6.75 7h12M6.75 7l-.621 9.967a2.25 2.25 0 01-2.244 2.077H8.622a2.25 2.25 0 01-2.244-2.077L6.75 7z"
         />
     </svg>
-    <!-- Messagerie (bulle) -->
-    <svg
-        v-else-if="name === 'chat'"
-        :class="props.class"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-    >
-        <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M7.5 8.25h9m-9 3H12m-4.5 3.75h9m-9-9h13.5a2.25 2.25 0 012.25 2.25v9a2.25 2.25 0 01-2.25 2.25H5.25l-3 3v-15a2.25 2.25 0 012.25-2.25z"
-        />
-    </svg>
+    <!-- Messagerie (SMS) -->
+    <span
+        v-else-if="name === 'chat' || name === 'chat-dot'"
+        :class="['inline-block shrink-0 bg-current opacity-90', props.class]"
+        :style="smsMaskStyle"
+        aria-hidden="true"
+    />
     <!-- Profil -->
     <svg
         v-else-if="name === 'user-circle'"
@@ -161,15 +166,6 @@ const props = defineProps({
             stroke-linejoin="round"
             d="m15.75 10.5 4.72-4.72a.75.75 0 0 1 1.28.53v11.38a.75.75 0 0 1-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25h-9A2.25 2.25 0 0 0 2.25 7.5v9a2.25 2.25 0 0 0 2.25 2.25Z"
         />
-    </svg>
-    <!-- Point non lu (messagerie) -->
-    <svg
-        v-else-if="name === 'chat-dot'"
-        :class="props.class"
-        viewBox="0 0 24 24"
-        fill="currentColor"
-    >
-        <circle cx="12" cy="12" r="6" />
     </svg>
     <!-- Alerte -->
     <svg
